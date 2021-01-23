db03d3d235073e50fc63fd4cdb1fb4b8f24e1410
gcaurin <gcaurin@gmail.com>
2021-01-23 20:50:29 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+// ****************************************************************************
 // Arquivo criado para estudo de conceitos C++ Moderno
-// Referencias podem ser encontradas em 
+// Autor - Mr. Grilo
+// Data - Janeiro de 2021
+// ****************************************************************************
+// Inspiracoes e referencias podem ser encontradas em 
+//
+// Video do Straustrop  CppCon 2017: 
+// Bjarne Stroustrup “Learning and Teaching Modern C++”
+// https://www.youtube.com/watch?v=fX2W3nNjJIo
+//
+// https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md
+// https://isocpp.org/blog/2015/09/bjarne-stroustrup-announces-cpp-core-guidelines
+// https://en.cppreference.com/w/
+// *****************************************************************************
+// Glauco gosta desse link
+// https://www.learncpp.com/
+//
+// *****************************************************************************
 
 #include <iostream>
 #include <typeindex>
